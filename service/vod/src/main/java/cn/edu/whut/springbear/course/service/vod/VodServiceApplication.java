676d8ab47670f2c951c94185eec024a4534189ee
Spring-_-Bear <springbear2020@163.com>
2022-10-24 13:02:18 +0800

    integrate the spring cloud gateway and nacos to service for the distribution functions

--- service/vod/src/main/java/cn/edu/whut/springbear/course/service/vod/VodServiceApplication.java
+++ service/vod/src/main/java/cn/edu/whut/springbear/course/service/vod/VodServiceApplication.java
@@ -2,12 +2,14 @@ package cn.edu.whut.springbear.course.service.vod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan("cn.edu.whut.springbear.course")
-public class ServiceVodApplication {
+public class VodServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceVodApplication.class, args);
+        SpringApplication.run(VodServiceApplication.class, args);
     }
 }
